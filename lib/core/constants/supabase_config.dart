587a2cf221b0e577cfa2b3f5265cbb199f6fcff1
Arnab Roy <106859594+arOy1011@users.noreply.github.com>
2026-08-07 12:39:40 +0530

    Auto sync 2026-08-07 12:39:40

---
+++ lib/core/constants/supabase_config.dart
@@ -0,0 +1,9 @@
+class SupabaseConfig {
+  SupabaseConfig._();
+
+  static const String url =
+      'https://sajmiqoaufrugwpnaekn.supabase.co';
+
+  static const String anonKey =
+      'sb_publishable_-jUm3sM9aTHshUdxfnyzVA_F6cDBAht';
+}
