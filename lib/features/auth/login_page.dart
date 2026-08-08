f004cc07e37ca49f18b38a442b78b406f8d30ef6
Arnab Roy <106859594+arOy1011@users.noreply.github.com>
2026-08-09 01:02:16 +0530

    Auto sync 2026-08-09 01:02:16

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -94,9 +94,10 @@ class _LoginPageState extends State<LoginPage> {
 
     try {
       final response = await Supabase.instance.client.auth.signUp(
-        email: email,
-        password: password,
-      );
+      email: email,
+      password: password,
+      emailRedirectTo: 'app.hostelmess.mess://login-callback',
+    );
 
       if (!mounted) return;
 
